7c03cf690c4a44904e5109acb48f8b13c3c44fa8
Miguel Bernal <miguelangelbernal1991@gmail.com>
2023-02-14 21:39:58 -0500

    Inicio Calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import ImagenCalculadora from '../src/imagenes/imagen-calculadora.avif';
+import  Boton from './componentes/Boton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img
+          src={ImagenCalculadora}
+          className='logo-contenedor'
+          alt='Logo Principal'
+        />  
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div> 
+        <div className='fila'>
+        </div>
+        <div className='fila'>
+        </div>  
+        <div className='fila'>
+        </div>
+        <div className='fila'>
+        </div>
+
+      </div>
     </div>
   );
 }
